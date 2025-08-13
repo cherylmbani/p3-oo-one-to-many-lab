bd63bde3f8c7c6756f2d1da4444adc6ed9d371b6
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-13 13:19:04 +0300

    one-to-many-object relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}. Must be one of {Pet.PET_TYPES} ")
+        else: self._pet_type = pet_type
+        
+    
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner ==self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception("Only Pet instances can be added.")
+
+    def get_sorted_pets(self):
+        owner_pets = self.pets()
+        for pet in owner_pets:
+            if not isinstance(pet, Pet):
+                raise Exception("Must be among the pet types")
+        return sorted(owner_pets, key=lambda p: p.name)
